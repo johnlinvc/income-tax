6a27e3673e3775ff3491868990e7bc5de71222c3
John Lin <johnlinvc@gmail.com>
2016-06-25 00:35:16 +0800

    Update Taiwan
    Update taiwan tax rate to the latest standard(2015 financial year)

--- lib/income_tax/countries/taiwan.rb
+++ lib/income_tax/countries/taiwan.rb
@@ -3,12 +3,13 @@ module IncomeTax
     class Taiwan < Models::Progressive
       register "Taiwan", "Taiwan, Province of China", "China, Republic of", "TW", "TWN"
       currency "TWD"
-
-      level   500_000, "5%"
-      level 1_130_000, "12%"
-      level 2_260_000, "20%"
-      level 4_230_000, "30%"
-      remainder        "40%"
+      level offset(218_000)  , "0%"
+      level offset(500_000)  , "5%"
+      level offset(650_000)  , "12%"
+      level offset(1_180_000), "20%"
+      level offset(2_050_000), "30%"
+      level offset(5_600_000), "40%"
+      remainder        "45%"
     end
   end
 end
